451bafcf4f136a8cd6633e3412c6743b314e7860
ramir512 <ramir.khvan@gmail.com>
2023-11-18 21:17:54 +0300

    Add more endpoints

--- main.go
+++ main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from Snippetbox!"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+	w.Write([]byte("Create snippet..."))
 }
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	log.Println("Starting server on: 4000")
-	err := http.ListenAndServe(":4000", mux)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+	log.Println("Starting server on: 80")
+	err := http.ListenAndServe(":80", mux)
 	log.Fatal(err)
 }
