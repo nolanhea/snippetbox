53e0f38b4c4be2dfadfc5406b020f80d2214d7b9
ramir512 <ramir.khvan@gmail.com>
2023-11-19 12:31:33 +0300

    Add custom loggers, cfg struct and http.Server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
 func main() {
+
+	cfg := config{}
+	flag.StringVar(&cfg.addr, "addr", ":4000", "Port to listen to")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Directory containing static variables")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
@@ -14,7 +30,7 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Println("Starting server on :80")
-	err := http.ListenAndServe(":80", mux)
-	log.Fatal(err)
+	infoLog.Println("Starting server on :80")
+	err := http.ListenAndServe(cfg.addr, mux)
+	errorLog.Fatal(err)
 }
