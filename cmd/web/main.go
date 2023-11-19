3d732229e9bcba2e013ac89ec7312da354131591
ramir512 <ramir.khvan@gmail.com>
2023-11-19 14:27:20 +0300

    Add routes.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -32,14 +32,7 @@ func main() {
 		infoLog: infoLog,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
+	mux := app.routes()
 	srv := &http.Server{
 		Addr:     cfg.addr,
 		ErrorLog: errorLog,
